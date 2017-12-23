878f3d24c2f2f2af72e0ece7576114383816d846
barlima <bartekperucki@gmail.com>
2017-12-23 16:27:01 +0100

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   # Accessors
 
   attr_accessor :remember_token
-
+  attr_accessor :reset_token
 
   # Validators
 
@@ -47,6 +47,21 @@ class User < ApplicationRecord
     BCrypt::Password.new(digest).is_password?(token)
   end
 
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns(reset_digest:  User.digest(reset_token), reset_sent_at: Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
 
   # Public methods
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
+        'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
     }
   else
     config.action_controller.perform_caching = false
@@ -26,12 +26,14 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
+
   config.action_mailer.delivery_method = :test
-  # ToDo: Domain name
-  host = '0.0.0.0:3000' # Don't use this literally; use your local dev host instead
+
+  host = 'localhost:3000' # Don't use this literally; use your local dev host instead
+
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
