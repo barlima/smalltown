fe3032e19d25f971f929153adf92b4bf1aca9fcc
barlima <bartekperucki@gmail.com>
2018-01-20 20:19:33 +0100

    Add temporary setadmin and setcreator action

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,11 @@ Rails.application.routes.draw do
   get '/about',   to:   'static_pages#about'
   get '/contact', to:   'static_pages#contact'
 
-  get '/signup',  to:   'users#new'
-  post '/signup', to:   'users#create'
+  get '/signup',    to:  'users#new'
+  post '/signup',   to:  'users#create'
+
+  post '/creator',  to:  'users#creator'
+  post '/admin',    to:  'users#admin'
 
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
