d12e9d4f43487f998ff99b4b73f9bec3a60233e6
barlima <bartekperucki@gmail.com>
2018-01-21 13:41:33 +0100

    Fix routes and conections between views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
-  before_action :correct_user,   only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update, :show]
   before_action :admin_user,     only: :destroy
 
 
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
 
   def creator
     @user = User.find(params[:id])
-    if @user.update_attribute(:creator, true)
+    if @user.update_attribute(:creator, !@user.creator)
       flash[:success] = 'User updated'
     else
       flash[:danger] = 'Cannot update selected user'
@@ -75,12 +75,6 @@ class UsersController < ApplicationController
   end
 
   # Before filters
-
-  # Confirms the correct user.
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to(root_url) unless current_user?(@user)
-  end
 
   # Confirms an admin user.
   def admin_user
