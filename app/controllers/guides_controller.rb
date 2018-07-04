644db67de70cabd7bba19f4a74895fbc47a8306f
barlima <bartekperucki@gmail.com>
2018-07-04 23:41:35 +0200

    Remove active option from guides_controller

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -2,16 +2,6 @@ class GuidesController < ApplicationController
 
   def index
     @guides = Guide.paginate(page: params[:page])
-    @active = params[:type] || 'list'
-
-    case params[:type]
-    when 'map'
-      @list_id = 'inactive'
-      @show_map_id = 'active'
-    else
-      @list_id = 'active'
-      @show_map_id = 'inactive'
-    end
   end
 
   def show
