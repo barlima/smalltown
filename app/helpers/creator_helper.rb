65387fe35979594b68ed635e3b01ee6db7893b30
barlima <bartekperucki@gmail.com>
2018-07-08 15:08:58 +0200

    Add cookie to determine which tab is currently active

--- app/helpers/creator_helper.rb
+++ app/helpers/creator_helper.rb
@@ -1,2 +1,9 @@
 module CreatorHelper
+  def points_active
+    return 'active in' if cookies[:active_tab] != 'paths'
+  end
+
+  def paths_active
+    return 'active in' if cookies[:active_tab] == 'paths'
+  end
 end
